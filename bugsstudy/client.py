27b12f2ab05121c164f158c55256680f3604e013
sisterofDC <yinxinwangfunny@qq.com>
2021-12-28 13:28:31 +0800

    socket study

--- bugsstudy/client.py
+++ bugsstudy/client.py
@@ -1,8 +1,8 @@
 import socket
 
 
-class UdpClient(object):
-    def tcpclient(self):
+class Client(object):
+    def udpclient(self):
         clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         while True:
             data = input()
@@ -13,7 +13,19 @@ class UdpClient(object):
                 break
         clientSock.close()
 
+    def tcpclient(self):
+        clientsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        clientsock.connect(('localhost',8090))
+        while True:
+            data = input()
+            clientsock.send(data.encode('utf-8'))
+            recvData = clientsock.recv(1024)
+            print(recvData)
+            if data == '0':
+                break
+        clientsock.close()
+
 
 if __name__ == "__main__":
-    udpClient = UdpClient()
+    udpClient = Client()
     udpClient.tcpclient()
--- bugsstudy/server.py
+++ bugsstudy/server.py
@@ -1,20 +1,55 @@
 import socket
+import ssl
 
-
-class UdpServer(object):
-    def tcpServer(self):
+class Server(object):
+    def udpServer(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(('', 9527))  # 绑定同一个域名下的所有机器
+        sock.bind(('', 9527))
         while True:
             revcData, (remoteHost, remotePort) = sock.recvfrom(1024)
-            print("[%s:%s] connect" % (remoteHost, remotePort),revcData)  # 接收客户端的ip, port
+            print("[%s:%s] connect" % (remoteHost, remotePort), revcData)  # 接收客户端的ip, port
             data = input()
             if data == '0':
                 break
             sock.sendto(data.encode('utf-8'), (remoteHost, remotePort))
         sock.close()
 
+    def tcpServer(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('localhost', 8090))
+        sock.listen(5)
+        conn, addr = sock.accept()
+        while True:
+            revcData = conn.recv(1024)
+            print(revcData, addr)
+            data = input()
+            conn.send(data.encode('utf-8'))
+            if data == '0':
+                break
+        sock.close()
+
+    def httpreqest(self):
+        s = ssl.wrap_socket(socket.socket())
+        s.connect(('www.bing.com', 443))
+        s.send(b'GET / HTTP/1.1\r\nHost: www.bing.com\r\nConnection: close\r\n\r\n')
+        # GET / article - types / 6 / HTTP / 1.1
+        # Host: www.zhangdongshengtech.com
+        # Connection: close
+        buffer = []
+        while True:
+            d = s.recv(1024)
+            if d:
+                buffer.append(d)
+            else:
+                break
+        data = b''.join(buffer)
+        s.close()
+        header, html = data.split(b'\r\n\r\n', 1)
+        print(header.decode('utf-8'))
+        with open('../savehtml/save.html','wb') as f:
+            f.write(html)
+
 
 if __name__ == "__main__":
-    udpServer = UdpServer()
-    udpServer.tcpServer()
+    udpServer = Server()
+    udpServer.httpreqest()
